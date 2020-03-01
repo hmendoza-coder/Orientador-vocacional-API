8e3f18773ba64b0b4e1f5357dbe6e72bbd41d9c3
Hector Miguel <stryke.r@hotmail.com>
2020-03-01 15:55:14 -0600

    Se suben el codigo fuente base

---
+++ Models/ValueSamples.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrientadorVocacionalAPI
+{
+    public static class ValueSamples
+    {
+        public static Dictionary<int, string> MyValue;
+
+        public static void Initialize()
+        {
+            MyValue = new Dictionary<int, string> {{0, "Value 0"}, {1, "Value 1"}, {2, "Value 2"}};
+        }
+    }
+}
